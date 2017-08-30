e75de1c73c8979de3da9df1904d1e4840d7605eb
rahulkulmi <rahul@newput.com>
2017-08-30 22:55:07 +0530

    Remove validation error.

--- app_util/exceptions.js
+++ app_util/exceptions.js
@@ -1,9 +1,5 @@
 'use strict';
 module.exports = {
-  NOT_AUTHORIZED: function(customMsg) {
-    return {'rcode': 401, 'httpCode': 401,
-      'message': 'No authorization', 'customMsg': customMsg};
-  },
   INTERNAL_SERVER_ERROR: function(customMsg) {
     return {'rcode': 500, 'httpCode': 500,
       'message': 'Server encountered some problem', 'customMsg': customMsg};
@@ -11,10 +7,6 @@ module.exports = {
   BAD_REQUEST: function(customMsg) {
     return {'rcode': 400, 'httpCode': 400,
       'message': 'Bad request', 'customMsg': customMsg};
-  },
-  VERIFICATION_EXCEPTION: function(customMsg) {
-    return {'rcode': 402, 'httpCode': 402,
-      'message': 'Verification exception.', 'customMsg': customMsg};
   },
   VALIDATION_EXCEPTION: function(rcode, customMsg) {
     return {'rcode': rcode, 'httpCode': 400,
